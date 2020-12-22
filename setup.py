7cbeba4539ccfcc88c8d87d7521a1698899efd2a
Will Traweek <will.traweek@gmail.com>
2020-12-22 04:15:14 -0600

    rename package and rework requirements

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CodeLighthouse_S",
+    name="easy-boto",
     version="1.0.0",
     author="CodeLighthouse",
     author_email="hello@codelighthouse.io",
-    description="CodeLighthouse's AWS S3 client",
+    description="An easy to use client for boto3",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/CodeLighthouse/aws-s3-client",
+    url="https://github.com/CodeLighthouse/easy-boto",
     packages=setuptools.find_packages(),
     license='MIT',
     python_requires='>=3.6',
     install_requires=[
-        'botocore',
         'boto3'
     ]
 )
