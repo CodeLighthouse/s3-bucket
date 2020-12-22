505e20ff0bd915ab5b402ae79c63e6ab2b0e2ce4
Kyle Mistele <kyle@mistele.com>
2020-12-21 20:14:52 -0500

    add example.py and empty setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CodeLighthouse_S",
+    version="1.1.0",
+    author="CodeLighthouseS3Client",
+    author_email="hello@codelighthouse.io",
+    description="CodeLighthouse's AWS S3 client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CodeLighthouse/aws-s3-client",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'botocore',
+        'boto3'
+    ]
+)
