c11947377b0446230efb3d4ba3d4a476814f9d6b
Will Traweek <will.traweek@gmail.com>
2020-12-22 15:20:58 -0600

    update example to fit new prepare paradigm

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-import s3_bucket as S3
+from s3_bucket import Bucket
 import os
 
 # get your key data from environment variables
@@ -6,10 +6,10 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 # initialize the package
-S3.init(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+Bucket.prepare(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
 # initialize a bucket
-my_bucket = S3.Bucket('my-bucket')
+my_bucket = Bucket('my-bucket')
 
 # some json string
 my_json_str = "{'a': 1, 'b': 2}" # an example json string
